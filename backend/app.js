f6b7b7e3550a620cb3bf90d275137f79b196e33f
manish0732 <manishjoshi0732@gmail.com>
2024-09-16 22:04:04 +0530

    1-user routes and controller added

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,34 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from 'cloudinary';
 
-import authRoutes from './routes/auth.routes.js';
-import connectToMOngoDB from './db/connectToMongoDB.js';
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMOngoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(morgan('dev')); // morgan for console level request tracking.
+app.use(morgan("dev")); // morgan for console level request tracking.
 
-app.use(express.json());  // to parse req.body
-app.use(express.urlencoded({ extended: true }))  // to parse form data
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data
 
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-    connectToMOngoDB();
+  console.log(`listening on port ${PORT}`);
+  connectToMOngoDB();
 });
