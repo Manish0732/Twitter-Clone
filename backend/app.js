8dcb0d92c112138768680d5b62e970a44e43309a
manish0732 <manishjoshi0732@gmail.com>
2024-10-03 19:48:21 +0530

    3-payload limit upgraded in app.js (backend req.body limit up to 5mb)

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(morgan("dev")); // morgan for console level request tracking.
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body   
 app.use(express.urlencoded({ extended: true })); // to parse form data
 
 app.use(cookieParser());
