b7cb93e0f5ae14588848ef913cadabde070313ef
manish0732 <manishjoshi0732@gmail.com>
2024-10-08 23:06:57 +0530

    3-Prepared for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(morgan("dev")); // morgan for console level request tracking.
 
@@ -31,6 +33,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get('*', async (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
